457614e84c1353d3f250de7e5020c1b45977d7b3
Henric Zazzi <hzazzi@kth.se>
2017-02-22 14:18:13 +0100

    added git hash to version control

---
+++ version.cmake
@@ -0,0 +1,20 @@
+###############################################################################
+# Set version
+###############################################################################
+
+set(VERSION_MAJOR "0")
+set(VERSION_MINOR "2")
+set(VERSION_PATCH "0")
+set(GIT_HASH "Unknown")
+find_package(Git)
+if(GIT_FOUND)
+  message(STATUS "Git found")
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} --no-pager show -s --pretty=format:%h -n 1
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+    )
+endif(GIT_FOUND)
+configure_file(version_config.h.in ${CMAKE_CURRENT_BINARY_DIR}/version_config.h)
